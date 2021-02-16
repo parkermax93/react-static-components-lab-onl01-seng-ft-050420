cfb495ff6dde6e4548dbdf5aa22600f2f8f027bf
parkermax93 <parker.williamson@gmail.com>
2021-02-16 05:18:07 +0000

    Fix mousecomponent error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CatComponent from './CatComponent.js'
 import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent.js'
+import MouseComponent from './MouseComponent.js'
 
 class App extends Component {
 	render() {
@@ -9,7 +10,7 @@ class App extends Component {
 			<div className="App">
 				<CatComponent />
 				<GraceHopperQuoteComponent />
-				{/* one more component missing */}
+				<MouseComponent />
 			</div>
 		);
 	}
